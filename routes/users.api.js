3b983e2dd31433ce6b279a5e419f08dca64f56f5
minhquanphan <57800230+minhquanphan@users.noreply.github.com>
2022-11-23 17:28:23 +0700

    addd validator

--- routes/users.api.js
+++ routes/users.api.js
@@ -1,19 +1,46 @@
 const express = require("express");
+const { param, body, header } = require("express-validator");
 const {
   register,
   login,
   updateProfile,
   changePassword,
-} = require("../controllers/user.controller");
+} = require("../controllers/user.controllers");
 const { loginRequired } = require("../middlewares/authentication");
+const { validate, checkObjectId } = require("../middlewares/validator");
 const router = express.Router();
 
-router.post("/register", register);
+router.post(
+  "/register",
+  validate([
+    body("name", "Invalid name").exists().notEmpty(),
+    body("email", "Invalid email").exists().isEmail(),
+    body("password", "Invalid password").exists().notEmpty(),
+  ]),
+  register
+);
 
-router.post("/login", login);
+router.post(
+  "/login",
+  validate([
+    body("email", "Invalid email").exists().isEmail(),
+    body("password", "Invalid password").exists().notEmpty(),
+  ]),
+  login
+);
 
-router.put("/profile", loginRequired, updateProfile);
+router.put(
+  "/profile",
+  loginRequired,
+  validate([body("name", "Invalid name").exists().notEmpty()]),
+  updateProfile
+);
 
-router.put("/password", loginRequired, changePassword);
+router.put(
+  "/password",
+  loginRequired,
+  validate([body("password", "Invalid password").exists().notEmpty()]),
+  changePassword
+);
 
 module.exports = router;
