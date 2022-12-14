77a6e9cb6db54b19b4d28b84fd4b0150a4b098ca
minhquanphan <57800230+minhquanphan@users.noreply.github.com>
2022-12-14 16:10:37 +0700

    finish main header and product page

---
+++ frontend/src/components/ProductCard.js
@@ -0,0 +1,49 @@
+import React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea, Stack } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { fCurrency } from "../utils/numberFormat";
+
+function ProductCard({ product }) {
+  const navigate = useNavigate();
+  return (
+    <Card onClick={() => navigate(`/product/${product.id}`)}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="200"
+          image={product.imageUrl}
+          alt={product.name}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="body1" component="div" noWrap>
+            {product.name}
+          </Typography>
+          <Stack
+            direction="row"
+            spacing={0.5}
+            alignItems="center"
+            justifyContent="flex-end"
+          >
+            {/* {product.price && (
+              <Typography
+                component="span"
+                sx={{ color: "text.disabled", textDecoration: "line-through" }}
+              >
+                {fCurrency(product.price)}
+              </Typography>
+            )} */}
+            <Typography variant="subtitle1">
+              {fCurrency(product.price)}
+            </Typography>
+          </Stack>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
+
+export default ProductCard;
